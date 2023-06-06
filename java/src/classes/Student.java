1e202937fd75665b5ad10eae8fc63c1b4d524f5a
nithinmadhavb <nithinmadhavb@gmail.com>
2023-06-06 18:33:07 -0400

    Added average and grade calculation feature in student.java

--- java/src/classes/Student.java
+++ java/src/classes/Student.java
@@ -5,18 +5,26 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String Stu_name;
-		int stu_id;
+		String studentName;
+		int studentID;
 		int age;
 		float fee;
 		boolean graduation;
-		System.out.println(gradeCalculation(5,3));
+		int subject1Marks = 90, subject2Marks = 23;
+		
+		System.out.println("Total marks: "+gradeCalculation(subject1Marks, subject2Marks));
+		averageCalculator(subject1Marks, subject2Marks);
 		
 	}
 
-	static int gradeCalculation(int sub1, int sub2) {
+	static void averageCalculator(int subject1Marks, int subject2Marks) {
 		// TODO Auto-generated method stub
-		return sub1+sub2;
+		System.out.print("Average score: "+(subject1Marks + subject2Marks)/2);
+	}
+
+	static int gradeCalculation(int subject1Marks, int subject2Marks) {
+		// TODO Auto-generated method stub
+		return subject1Marks + subject2Marks;
 	}
 
 }
