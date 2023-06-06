ba4dcc20ff7c2e482d57518e18b474678ddfcbd2
nithinmadhavb <nithinmadhavb@gmail.com>
2023-06-06 12:08:04 -0400

    adding Student.java file

---
+++ java/src/classes/Student.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Stu_name;
+		int stu_id;
+		int age;
+		float fee;
+		boolean graduation;
+
+
+	}
+
+}
